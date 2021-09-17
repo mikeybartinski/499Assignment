78e1507f4b3487e0fc3128222fd40bbbb909b12b
mikeybartinski <mikey9414@gmail.com>
2021-09-17 01:26:34 -0700

    stringSort update

--- stringSort.py
+++ stringSort.py
@@ -1,8 +1,8 @@
-# Python program to sort a list of strings
 
-lst = ['gfg', 'is', 'a', 'portal', 'for', 'geeks']
 
-# Using sort() function
+lst = ['for', 'science', 'a', 'computer', 'this', 'is', 'class']
+
+
 lst.sort()
 
 print(lst)
