6c8691adf5173eb45971591181518f555e29e2af
mikeybartinski <43162591+mikeybartinski@users.noreply.github.com>
2021-09-17 00:42:50 -0700

    Add files via upload

---
+++ array.py
@@ -0,0 +1,15 @@
+arr = [1,2,3,4,5,6,7]
+x= 0
+length = len(arr) - 1
+print(len(arr))
+
+while x < len(arr)/2:
+    temp1 = arr[length - x]
+    temp2 = arr[x]
+    arr[x] = temp1
+    arr[length - x] = temp2
+    x += 1
+
+print (arr)
+
+
